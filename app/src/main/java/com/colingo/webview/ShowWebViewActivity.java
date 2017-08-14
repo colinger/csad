88aa4f8d8141a8c8f4a4aa55d282a736c069e463
葛新颖 <gxy23996@ly.com>
2017-08-14 22:03:38 +0800

    新版本

--- app/src/main/java/com/colingo/webview/ShowWebViewActivity.java
+++ app/src/main/java/com/colingo/webview/ShowWebViewActivity.java
@@ -2,12 +2,10 @@ package com.colingo.webview;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.webkit.WebSettings;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
 import com.example.app.R;
-
 
 public class ShowWebViewActivity extends Activity {
 
@@ -18,18 +16,22 @@ public class ShowWebViewActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_web_main);
         mWebView = (WebView) findViewById(R.id.activity_main_webview);
-        // Force links and redirects to open in the WebView instead of in a browser
-        mWebView.setWebViewClient(new WebViewClient());
-        // Enable Javascript
-        WebSettings webSettings = mWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-         mWebView.loadUrl("http://gw.cs.cn/jx.html");
+
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setDomStorageEnabled(true);
+        mWebView.getSettings().setSupportMultipleWindows(true);
+        mWebView.getSettings().setLoadWithOverviewMode(true);
+        mWebView.getSettings().setBlockNetworkImage(false);
+        mWebView.getSettings().setUseWideViewPort(true);
+
+        mWebView.setWebChromeClient(new WebChromeClient());
+        mWebView.loadUrl("http://gw.cs.cn/jx.html");
     }
 
     // Prevent the back-button from closing the app
     @Override
     public void onBackPressed() {
-        if(mWebView.canGoBack()) {
+        if (mWebView.canGoBack()) {
             mWebView.goBack();
         } else {
             super.onBackPressed();
