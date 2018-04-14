963fa90b94b3ef091ff749dfc8c01a84f6fa1f52
colin <colin.gxy@gmail.com>
2018-04-14 15:55:36 +0800

    update

--- app/src/main/java/com/colingo/webview/MainActivity.java
+++ app/src/main/java/com/colingo/webview/MainActivity.java
@@ -1,30 +1,74 @@
 package com.colingo.webview;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.baidu.kfk.wv.AdUtils;
 import com.baidu.kfk.wv.NotificationBroadcastReceiver;
 import com.example.app.R;
+import com.netease.scan.IScanModuleCallBack;
+import com.netease.scan.QrScan;
+import com.netease.scan.ui.CaptureActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
+    private CaptureActivity mCaptureContext;
+    private Button mStartScanButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //
+        mStartScanButton = (Button)findViewById(R.id.btn_start_scan);
 
-        AdUtils.showAd(this.findViewById(android.R.id.content)
-                , this);
+//        AdUtils.showAd(this.findViewById(android.R.id.content)
+//                , this);
 
+        mStartScanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                QrScan.getInstance().launchScan(MainActivity.this, new IScanModuleCallBack() {
+                    @Override
+                    public void OnReceiveDecodeResult(final Context context, String result) {
+                        mCaptureContext = (CaptureActivity)context;
+
+                        AlertDialog dialog = new AlertDialog.Builder(mCaptureContext)
+                                .setMessage(result)
+                                .setCancelable(false)
+                                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                        QrScan.getInstance().restartScan(mCaptureContext);
+                                    }
+                                })
+                                .setPositiveButton("关闭", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                        QrScan.getInstance().finishScan(mCaptureContext);
+                                    }
+                                })
+                                .create();
+                        dialog.show();
+                    }
+                });
+            }
+        });
     }
 
     private void showNotification(String msg){
---
+++ app/src/main/java/com/colingo/webview/WVApplication.java
@@ -0,0 +1,34 @@
+package com.colingo.webview;
+
+import android.app.Application;
+
+import com.example.app.R;
+import com.netease.scan.QrScan;
+import com.netease.scan.QrScanConfiguration;
+
+/**
+ * Created by colingo on 28/11/2017.
+ */
+
+public class WVApplication extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        QrScanConfiguration configuration = new QrScanConfiguration.Builder(this)
+                .setTitleHeight(53)
+                .setTitleText("来扫一扫")
+                .setTitleTextSize(18)
+                .setTitleTextColor(R.color.white)
+                .setTipText("将二维码放入框内扫描~")
+                .setTipTextSize(14)
+                .setTipMarginTop(40)
+                .setTipTextColor(R.color.white)
+                .setSlideIcon(R.drawable.capture_add_scanning)
+                .setAngleColor(R.color.white)
+                .setMaskColor(R.color.black_80)
+                .setScanFrameRectRate((float) 0.8)
+                .build();
+        QrScan.getInstance().init(configuration);
+    }
+}
